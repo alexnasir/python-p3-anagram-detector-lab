f39d5a3446d2e790f7edfab5bd4912be1d430e50
alexnasir <alexnasiali45@gmail.com>
2024-11-30 23:54:35 +0300

    commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+    
+    def match(self, possible_anagrams):
+        return [word for word in possible_anagrams if sorted(word) == self.sorted_word]
